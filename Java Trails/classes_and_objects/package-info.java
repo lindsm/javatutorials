9ba95683ed5a287b60795cc702f44c4f555da001
lindsm <lindsm@gmail.com>
2014-10-19 17:13:08 -0700

    next chapter

---
+++ Java Trails/classes_and_objects/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Trail: Learning the Java Language: Table of Contents
+ * http://docs.oracle.com/javase/tutorial/java/javaOO/index.html
+ */
+/**
+ * @author Mike
+ *
+ */
+package classes_and_objects;
